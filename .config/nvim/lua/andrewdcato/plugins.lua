d7909a8ac214e48d6ecc72cde1bcd2849d9a3762
Andrew Cato <andrewdcato@gmail.com>
2023-01-09 22:59:23 -0600

    feat: start setting up lsp config

--- .config/nvim/lua/andrewdcato/plugins.lua
+++ .config/nvim/lua/andrewdcato/plugins.lua
@@ -66,6 +66,30 @@ return packer.startup(function(use)
 	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- improves sorting perf
 	use({ "nvim-telescope/telescope.nvim", tag = "0.1.0" })
 
+	-- Autocomplete and LSP
+	use({
+		'VonHeikemen/lsp-zero.nvim',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-buffer'},
+			{'hrsh7th/cmp-path'},
+			{'saadparwaiz1/cmp_luasnip'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'hrsh7th/cmp-nvim-lua'},
+
+			-- Snippets
+			{'L3MON4D3/LuaSnip'},
+			-- Snippet Collection (Optional)
+			{'rafamadriz/friendly-snippets'},
+		}
+	})
+
 	if packer_boostrap then
 		require("packer").sync()
 	end
