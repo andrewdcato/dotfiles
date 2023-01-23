a3cf1eba8251526108d0d9742f175b16a2870302
Andrew Cato <andrewdcato@gmail.com>
2023-01-22 22:32:50 -0600

    feat: set up a few telescope extensions

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -8,15 +8,47 @@ if not actions_configured then
 	return
 end
 
+local extensions = {
+	"fzf",
+	"file_browser",
+	"packer",
+}
+
 telescope.setup({
+	extensions,
 	defaults = {
 		mappings = {
 			i = {
-				["<C-k>"] = actions.move_selection_previous,
+				["<C-n>"] = actions.cycle_history_next,
+				["<C-p>"] = actions.cycle_history_prev,
+
 				["<C-j>"] = actions.move_selection_next,
-			}
-		}
-	}
+				["<C-k>"] = actions.move_selection_previous,
+
+				["<C-c>"] = actions.close,
+
+				["<CR>"] = actions.select_default,
+				["<C-x>"] = actions.select_horizontal,
+				["<C-v>"] = actions.select_vertical,
+				["<C-t>"] = actions.select_tab,
+
+				["<C-u>"] = actions.preview_scrolling_up,
+				["<C-d>"] = actions.preview_scrolling_down,
+
+				["<PageUp>"] = actions.results_scrolling_up,
+				["<PageDown>"] = actions.results_scrolling_down,
+
+				["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+				["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+				["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+				["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+				["<C-l>"] = actions.complete_tag,
+				["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+			},
+		},
+	},
 })
 
 telescope.load_extension("fzf")
+telescope.load_extension("file_browser")
+telescope.load_extension("packer")
--- .config/nvim/lua/andrewdcato/plugins.lua
+++ .config/nvim/lua/andrewdcato/plugins.lua
@@ -32,7 +32,7 @@ packer.init({
 		open_fn = function()
 			return require("packer.util").float({
 				border = "rounded",
-				style = "minimal"
+				style = "minimal",
 			})
 		end,
 	},
@@ -42,79 +42,81 @@ return packer.startup(function(use)
 	-- Packer is magic and can manage itself
 	use("wbthomason/packer.nvim")
 
-  -- Funcs needed by many plugins
+	-- Funcs needed by many plugins
 	use("nvim-lua/plenary.nvim")
-  use("nvim-lua/popup.nvim")
+	use("nvim-lua/popup.nvim")
 
 	-- Theming
 	use({
 		"nvim-treesitter/nvim-treesitter",
 		run = function()
-			pcall(require("nvim-treesitter.install").update { with_sync = true })
+			pcall(require("nvim-treesitter.install").update({ with_sync = true }))
 		end,
 	})
 
 	use({ "nvim-treesitter/nvim-treesitter-textobjects", after = "nvim-treesitter" })
 	use("kyazdani42/nvim-web-devicons")
 	use("ellisonleao/gruvbox.nvim")
-  use("EdenEast/nightfox.nvim")
+	use("EdenEast/nightfox.nvim")
 
 	-- Statusline
-  use("nanozuki/tabby.nvim")
-  use("feline-nvim/feline.nvim")
+	use("nanozuki/tabby.nvim")
+	use("feline-nvim/feline.nvim")
 
-  -- Git plugins
-	use("f-person/git-blame.nvim")	-- inline git blame
+	-- Git plugins
+	use("f-person/git-blame.nvim") -- inline git blame
 	use("kdheepak/lazygit.nvim")
 	use({
 		"lewis6991/gitsigns.nvim",
 		config = function()
-			require('gitsigns').setup()
-		end
+			require("gitsigns").setup()
+		end,
 	})
 
 	-- General workflow
 	use("nvim-tree/nvim-tree.lua")
 	use("numToStr/Comment.nvim")
-  use("folke/which-key.nvim")
+	use("folke/which-key.nvim")
 	use("windwp/nvim-autopairs")
-  use("norcalli/nvim-colorizer.lua")
+	use("norcalli/nvim-colorizer.lua")
 
-	-- Fuzzy finder
+	-- Telescope
+	use("nvim-telescope/telescope-packer.nvim")
+	use("nvim-telescope/telescope-file-browser.nvim")
 	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- improves sorting perf
 	use({ "nvim-telescope/telescope.nvim", tag = "0.1.0" })
 
-  -- LSP and Formatting
-  use({ "neovim/nvim-lspconfig" })
-  use({ "williamboman/mason-lspconfig.nvim" })
-  use({ "williamboman/mason.nvim" })
-  use({ "jose-elias-alvarez/null-ls.nvim" })
-  use({ "RRethy/vim-illuminate" })
+	-- LSP and Formatting
+	use({ "neovim/nvim-lspconfig" })
+	use({ "williamboman/mason-lspconfig.nvim" })
+	use({ "williamboman/mason.nvim" })
+	use({ "jose-elias-alvarez/null-ls.nvim" })
+	use({ "RRethy/vim-illuminate" })
 
-  -- Autocompletion
-  use("hrsh7th/nvim-cmp")
-  use("hrsh7th/cmp-buffer")
-  use("hrsh7th/cmp-path")
-  use("saadparwaiz1/cmp_luasnip")
-  use("hrsh7th/cmp-nvim-lsp")
-  use("hrsh7th/cmp-nvim-lua")
+	-- Autocompletion
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("saadparwaiz1/cmp_luasnip")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-nvim-lua")
 
-  -- Snippets
-  use("L3MON4D3/LuaSnip")
-  use("rafamadriz/friendly-snippets")
+	-- Snippets
+	use("L3MON4D3/LuaSnip")
+	use("rafamadriz/friendly-snippets")
 
-  -- Debugger
-  use("mfussenegger/nvim-dap")
-  use("mxsdev/nvim-dap-vscode-js")
-  use("rcarriga/nvim-dap-ui")
-  use("theHamsta/nvim-dap-virtual-text")
-  use("nvim-telescope/telescope-dap.nvim")
-  use({
-    "microsoft/vscode-js-debug",
-    opt = true,
-    run = "npm install --legacy-peer-deps && npm run compile",
-    tag = 'v1.74.1'  -- you *must* specify this tag; newer versions have breaking bugs
-  })
+	-- Debugger
+	use("mfussenegger/nvim-dap")
+	use("mxsdev/nvim-dap-vscode-js")
+	use("rcarriga/nvim-dap-ui")
+	use("theHamsta/nvim-dap-virtual-text")
+	use("nvim-telescope/telescope-dap.nvim")
+	use({
+		"microsoft/vscode-js-debug",
+		opt = true,
+		run = "npm install --legacy-peer-deps && npm run compile",
+		tag = "v1.74.1", -- you *must* specify this tag; newer versions have breaking bugs
+	})
 
 	if packer_bootstrap then
 		require("packer").sync()
