3ddd11a8bf8169e19ba6581be5e6fc88b15b0c8c
Andrew Cato <andrewdcato@gmail.com>
2023-01-27 10:44:36 -0600

    chore: keep playing around with lualine

--- .config/nvim/lua/andrewdcato/plugins/lualine.lua
+++ .config/nvim/lua/andrewdcato/plugins/lualine.lua
@@ -44,6 +44,41 @@ navic.setup({
 	separator = "  ",
 })
 
+local custom_fname = require("lualine.components.filename"):extend()
+local highlight = require("lualine.highlight")
+local default_status_colors = { saved = "#228B22", modified = "#C70039" }
+
+function custom_fname:init(options)
+	custom_fname.super.init(self, options)
+
+	self.status_colors = {
+		saved = highlight.create_component_highlight_group(
+			{ fg = default_status_colors.saved },
+			"filename_status_saved",
+			self.options
+		),
+		modified = highlight.create_component_highlight_group(
+			{ fg = default_status_colors.modified },
+			"filename_status_modified",
+			self.options
+		),
+	}
+
+	if self.options.color == nil then
+		self.options.color = ""
+	end
+end
+
+function custom_fname:update_status()
+	local data = custom_fname.super.update_status(self)
+
+	data = highlight.component_format_highlight(
+		vim.bo.modified and self.status_colors.modified or self.status_colors.saved
+	) .. data
+
+	return data
+end
+
 local function diff_source()
 	local gitsigns = vim.b.gitsigns_status_dict
 	if gitsigns then
@@ -63,7 +98,7 @@ local diff_component = {
 }
 
 local navic_component = {
-	"filename",
+	custom_fname,
 	{
 		navic.get_location,
 		cond = navic.is_available,
@@ -72,7 +107,7 @@ local navic_component = {
 }
 
 local disabled_filetypes = {
-	statusline = { "alpha", "lspsagaoutline" },
+	statusline = { "Alpha", "lspsagaoutline" },
 	winbar = { "alpha", "lspsagaoutline" },
 }
 
