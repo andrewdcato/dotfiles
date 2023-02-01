f71576b2c4fbd853d1eb3177c346a16f715af151
Andrew Cato <andrewdcato@gmail.com>
2023-02-01 09:14:44 -0600

    fix: colorscheme goes first so other plugins can use

--- .config/nvim/lua/andrewdcato/plugins/theme/init.lua
+++ .config/nvim/lua/andrewdcato/plugins/theme/init.lua
@@ -1,3 +1,3 @@
+require("andrewdcato.plugins.theme.colorscheme")
 require("andrewdcato.plugins.theme.modes")
 require("andrewdcato.plugins.theme.indent-blankline")
-require("andrewdcato.plugins.theme.colorscheme")
