816bd174b723b7a972ff7626d365bd066068e9ef
Andrew Cato <andrewdcato@gmail.com>
2023-02-02 15:51:57 -0600

    feat: update gitsigns config

--- .config/nvim/lua/andrewdcato/plugins/theme/init.lua
+++ .config/nvim/lua/andrewdcato/plugins/theme/init.lua
@@ -1,3 +1,4 @@
 require("andrewdcato.plugins.theme.colorscheme")
 require("andrewdcato.plugins.theme.modes")
 require("andrewdcato.plugins.theme.indent-blankline")
+require("andrewdcato.plugins.theme.gitsigns")
