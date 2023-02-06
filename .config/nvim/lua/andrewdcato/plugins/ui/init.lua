e50abe1bc8b2b8288a0577e438bc076ea2bcd2c7
Andrew Cato <andrewdcato@gmail.com>
2023-02-05 22:34:56 -0600

    chore: rename directory

---
+++ .config/nvim/lua/andrewdcato/plugins/ui/init.lua
@@ -0,0 +1,15 @@
+-- Set our colorscheme
+require("andrewdcato.plugins.ui.colorscheme")
+
+-- Set up buffer and statuslines
+require("andrewdcato.plugins.ui.statusline")
+require("andrewdcato.plugins.ui.bufferline")
+
+-- Filetree
+require("andrewdcato.plugins.ui.nvim-tree")
+
+-- Set up UI decoration plugins
+require("andrewdcato.plugins.ui.modes")
+require("andrewdcato.plugins.ui.indent-blankline")
+require("andrewdcato.plugins.ui.gitsigns")
+require("andrewdcato.plugins.ui.colorizer")
