e1611778d3c369ccfb47fd7311882b9cb80f8423
Andrew Cato <andrewdcato@gmail.com>
2023-02-14 10:10:40 -0600

    fix: scope tailwind and cssmodule lang servers

---
+++ .config/nvim/lua/andrewdcato/plugins/lsp/settings/tailwindcss.lua
@@ -0,0 +1,16 @@
+return {
+	filetypes = {
+		"css",
+		"html",
+		"javascript.jsx",
+		"javascriptreact",
+		"less",
+		"postcss",
+		"sass",
+		"scss",
+		"stylus",
+		"svelte",
+		"typescript.tsx",
+		"typescriptreact",
+	},
+}
