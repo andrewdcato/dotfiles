bef99e1f4ec07cb7b44b97cef652760e4449278a
Andrew Cato <andrewdcato@gmail.com>
2023-07-24 10:23:26 -0500

    feat!: hide tabline when only one tab

--- .config/wezterm/tab.lua
+++ .config/wezterm/tab.lua
@@ -119,7 +119,7 @@ function Tab.setup(config)
 	config.use_fancy_tab_bar = false
 	config.show_new_tab_button_in_tab_bar = false
 	config.tab_max_width = 50
-	config.hide_tab_bar_if_only_one_tab = false
+	config.hide_tab_bar_if_only_one_tab = true
 
 	wezterm.on("format-tab-title", function(tab)
 		return wezterm.format({
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,12 +4,27 @@ local tab = require("tab")
 local config = {}
 
 config.color_scheme = "Catppuccin Macchiato"
+config.enable_scroll_bar = false
 config.window_background_opacity = 0.9
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.ssh_domains = {
+	{
+		name = "anton",
+		remote_address = "127.0.0.1",
+		username = "andrewcato",
+	},
+}
 
 config.line_height = 1.05
 config.font_size = 16
 config.font = wezterm.font_with_fallback({
-	"Operator Mono Lig Book",
+	"OperatorMonoLig Nerd Font",
 	"JetBrainsMono Nerd Font Mono",
 	"Hack Nerd Font",
 })
