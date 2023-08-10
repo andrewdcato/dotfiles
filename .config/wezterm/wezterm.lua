8f6368210f560c377ceebfc2725c5e1a095133b5
Andrew Cato <andrewdcato@gmail.com>
2023-08-10 11:52:31 -0500

    chore: clean up opacity and colors

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,28 +5,20 @@ local config = {}
 
 config.color_scheme = "Catppuccin Macchiato"
 config.enable_scroll_bar = false
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.94
 config.window_padding = {
-	left = 0,
+	left = 5,
 	right = 0,
 	top = 0,
 	bottom = 0,
-}
-
-config.ssh_domains = {
-	{
-		name = "anton",
-		remote_address = "127.0.0.1",
-		username = "andrewcato",
-	},
 }
 
 config.line_height = 1.05
 config.font_size = 16
 config.font = wezterm.font_with_fallback({
 	"OperatorMonoLig Nerd Font",
-	"JetBrainsMono Nerd Font Mono",
-	"Hack Nerd Font",
+	"Hack Nerd Font Mono",
+	"icomoon",
 })
 
 config.keys = {
