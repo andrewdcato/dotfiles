fdd9f4241068e6370e7a8a9fc198c55351e3b6e2
Andrew Cato <andrewdcato@gmail.com>
2024-02-23 22:45:01 -0600

    feat(sketchybar): initial commit

---
+++ .config/sketchybar/utils/colors.sh
@@ -0,0 +1,34 @@
+# NOTE: Sketchybar expects colors in <argb_hex> - 0xff denotes 100% opacity.
+#       Helper for opacity prefixes here: https://davidwalsh.name/hex-opacity
+
+# Catppuccin Macchiato
+export ROSEWATER=0xfff4dbd6
+export FLAMINGO=0xfff0c6c6
+export PINK=0xfff5bde6
+export MAUVE=0xffc6a0f6
+export RED=0xffed8796
+export MAROON=0xffee99a0
+export PEACH=0xffee99a0
+export YELLOW=0xffeed49f
+export GREEN=0xffa6da95
+export TEAL=0xff8bd5ca
+export SKY=0xff8bd5ca
+export SAPPHIRE=0xff7dc4e4
+export BLUE=0xff8aadf4
+export LAVENDER=0xffb7bdf8
+export TEXT=0xffcad3f5
+export SUBTEXT1=0xffb8c0e0
+export SUBTEXT0=0xffa5adcb
+export OVERLAY2=0xff939ab7
+export OVERLAY1=0xff8087a2
+export OVERLAY0=0xff6e738d
+export SURFACE2=0xff5b6078
+export SURFACE1=0xff494d64
+export SURFACE0=0xff363a4f
+export BASE=0xff24273a
+export MANTLE=0xff1e2030
+export CRUST=0xff181926
+
+# Helpers
+export TRANSPARENT=0x00000000
+export BAR_BG=0x4D181926 # CRUST w/30% opacity
