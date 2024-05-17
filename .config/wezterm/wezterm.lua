17af79bc0aa610be20081f47fd799e2ad257ec70
Andrew Cato <andrewdcato@gmail.com>
2024-05-16 22:24:32 -0500

    chore(wezterm): nord

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,27 +3,17 @@ local tab = require("tab")
 
 local config = {}
 
-config.color_scheme = "Catppuccin Macchiato"
+config.term = "xterm-256color"
+config.color_scheme = "nordfox"
 config.enable_scroll_bar = false
-config.window_background_opacity = 0.87
-config.macos_window_background_blur = 90
+config.window_background_opacity = 0.80
+config.macos_window_background_blur = 80
 config.window_padding = {
 	left = 5,
 	right = 0,
 	top = 0,
 	bottom = 0,
 }
-
--- config.window_frame = {
--- 	border_left_width = "0.125cell",
--- 	border_right_width = "0.125cell",
--- 	border_bottom_height = "0.125cell",
--- 	border_top_height = "0.125cell",
--- 	border_left_color = "purple",
--- 	border_right_color = "purple",
--- 	border_bottom_color = "purple",
--- 	border_top_color = "purple",
--- }
 
 config.line_height = 1.05
 config.font_size = 16
