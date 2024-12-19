aa651c9344a17a6fedd59fe743e7be4b331259a5
Andrew Cato <andrewdcato@gmail.com>
2024-12-19 14:44:34 -0600

    feat(bar): add focus and amphetamine to info area

---
+++ .config/sketchybar/items/amphetamine.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# TODO: padding, toggle on and off on click?
+sketchybar --add alias "Amphetamine,Amphetamine" right \
+           --set "Amphetamine,Amphetamine" alias.color=$YELLOW
+
---
+++ .config/sketchybar/items/focus.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# TODO: padding, figure out click handler...
+sketchybar --add alias "Control Center,FocusModes" right \
+           --set "Control Center,FocusModes" alias.color=$CYAN
