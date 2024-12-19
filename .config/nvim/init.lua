9e1a1441e681f495a41b422fa6c2066eff0cac5a
Andrew Cato <andrewdcato@gmail.com>
2024-12-19 14:43:45 -0600

    refactor(nvim): move bootstrap to init

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,5 +7,20 @@ require("cato.core.options")
 -- Autocommands
 require("cato.core.autocommands")
 
+-- Bootstrap lazy.nvim if it is missing
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+
+vim.opt.rtp:prepend(lazypath)
+
 -- Lazy.nvim config
 require("cato.lazy")
--- .config/nvim/lua/cato/lazy.lua
+++ .config/nvim/lua/cato/lazy.lua
@@ -1,18 +1,3 @@
--- Bootstrap lazy.nvim if it is missing
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-
-vim.opt.rtp:prepend(lazypath)
-
 require("lazy").setup({
 	{ import = "cato.plugins" },
 	{ import = "cato.plugins.lsp" },
@@ -27,12 +12,7 @@ require("lazy").setup({
 			width = 0.9,
 			height = 0.9,
 		},
-		icons = {
-			ft = "",
-			lazy = "󰂠 ",
-			loaded = "",
-			not_loaded = "",
-		},
+		icons = require("cato.util").icons.lazy,
 	},
 	performance = {
 		rtp = {
