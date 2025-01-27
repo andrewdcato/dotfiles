0a1e20c01111b75f35b24f79e0013dd40297c800
Andrew Cato <andrewdcato@gmail.com>
2025-01-27 11:00:52 -0600

    fix(nvim): update url for bb pipelines schema

--- .config/nvim/lua/cato/plugins/lsp/settings/yamlls.lua
+++ .config/nvim/lua/cato/plugins/lsp/settings/yamlls.lua
@@ -6,7 +6,7 @@ return {
 			format = "enable",
 			keyOrdering = false,
 			schemas = {
-				["https://bitbucket.org/atlassianlabs/atlascode/raw/main/resources/schemas/pipelines-schema.json"] = "bitbucket-pipelines.*",
+				["https://api.bitbucket.org/schemas/pipelines-configuration"] = "bitbucket-pipelines.*",
 				["https://json.schemastore.org/github-action.json"] = ".github/actions/*",
 				["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
 				["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose.*",
