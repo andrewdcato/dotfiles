c8289a5acb2ccbed08dfe05204ce0a4cfad0943f
Andrew Cato <andrewdcato@gmail.com>
2025-04-21 10:34:33 -0500

    chore: configure undercurl

--- .config/nvim/lua/cato/core/options.lua
+++ .config/nvim/lua/cato/core/options.lua
@@ -34,9 +34,9 @@ local options = {
 	-- modeline
 	modeline = true,
 	modelines = 5,
-	-- folding
-	-- foldmethod = "expr",
-	-- foldexpr = "nvim_treesitter#foldexpr()",
+	-- spell check
+	-- spell = true,
+	-- spelllang = { "en_us" },
 }
 
 for key, value in pairs(options) do
@@ -47,6 +47,8 @@ end
 vim.opt.clipboard:append("unnamedplus") -- use the system clipboard when yanking text
 
 -- Misc
+vim.cmd([[let &t_Cs = "\e[4:3m"]]) -- Undercurl
+vim.cmd([[let &t_Ce = "\e[4:0m"]]) -- Undercurl
 vim.opt.guicursor = "a:block-Cursor" -- block cursor in all modes
 vim.opt.guicursor = "i-v:blinkon10" -- blink the cursor in insert or visual mode
 vim.opt.iskeyword:append("-") -- changes the "-" character to be considered part of the word
@@ -54,3 +56,4 @@ vim.opt.iskeyword:append("-") -- changes the "-" character to be considered part
 vim.api.nvim_set_option("updatetime", 300)
 vim.api.nvim_set_option("list", false)
 vim.api.nvim_set_option("splitright", true)
+vim.api.nvim_set_option("laststatus", 3)
