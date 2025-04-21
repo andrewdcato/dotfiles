2a57f7e73cb61f31ce1f6948db9c45d2bacc8951
Andrew Cato <andrewdcato@gmail.com>
2025-04-21 10:35:30 -0500

    chore: mocha

--- .config/nvim/lua/cato/plugins/modes.lua
+++ .config/nvim/lua/cato/plugins/modes.lua
@@ -2,7 +2,7 @@ return {
 	"mvllow/modes.nvim",
 	dependencies = { "catppuccin/nvim" },
 	config = function()
-		local colors = require("catppuccin.palettes").get_palette("macchiato")
+		local colors = require("catppuccin.palettes").get_palette("mocha")
 
 		require("modes").setup({
 			colors = {
@@ -11,18 +11,11 @@ return {
 				insert = colors.green,
 				visual = colors.maroon,
 			},
-			-- Set opacity for cursorline and number background
 			line_opacity = 0.35,
-			-- Enable cursor highlights
 			set_cursor = true,
-			-- Enable cursorline initially, and disable cursorline for inactive windows
-			-- or ignored filetypes
 			set_cursorline = true,
-			-- Enable line number highlights to match cursorline
 			set_number = true,
-			-- Disable modes highlights in specified filetypes
-			-- Please PR commonly ignored filetypes
-			ignore_filetypes = { "neo-tree", "TelescopePrompt", "lspsagaoutline" },
+			ignore_filetypes = { "neo-tree", "TelescopePrompt" },
 		})
 	end,
 }
