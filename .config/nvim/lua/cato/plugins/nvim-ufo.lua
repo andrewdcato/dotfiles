445f8cac4efa90341d46353eb0e46fe10689442e
Andrew Cato <andrewdcato@gmail.com>
2025-09-29 11:40:24 -0500

    refactor(nvim): clean up ufo config

--- .config/nvim/lua/cato/plugins/nvim-ufo.lua
+++ .config/nvim/lua/cato/plugins/nvim-ufo.lua
@@ -1,26 +1,72 @@
 return {
 	"kevinhwang91/nvim-ufo",
-	dependencies = { "kevinhwang91/promise-async" },
-	VeryLazy = true,
-	config = function()
-		local ufo = require("ufo")
-
-		vim.o.foldcolumn = "1" -- '0' is not bad
+	dependencies = "kevinhwang91/promise-async",
+	event = "VeryLazy",
+	opts = {
+		open_fold_hl_timeout = 400,
+		close_fold_kinds = { "imports", "comment" },
+		preview = {
+			win_config = {
+				border = { "", "─", "", "", "", "─", "", "" },
+				winhighlight = "Normal:Folded",
+				winblend = 0,
+			},
+			mappings = {
+				scrollU = "<C-u>",
+				scrollD = "<C-d>",
+				jumpTop = "[",
+				jumpBot = "]",
+			},
+		},
+	},
+	init = function()
+		vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+		vim.o.foldcolumn = "0" -- '0' is not bad``
 		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 		vim.o.foldlevelstart = 99
 		vim.o.foldenable = true
+	end,
+	config = function(_, opts)
+		local handler = function(virtText, lnum, endLnum, width, truncate)
+			local newVirtText = {}
+			local totalLines = vim.api.nvim_buf_line_count(0)
+			local foldedLines = endLnum - lnum
+			local suffix = ("  %d %d%%"):format(foldedLines, foldedLines / totalLines * 100)
+			local sufWidth = vim.fn.strdisplaywidth(suffix)
+			local targetWidth = width - sufWidth
+			local curWidth = 0
+			for _, chunk in ipairs(virtText) do
+				local chunkText = chunk[1]
+				local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+				if targetWidth > curWidth + chunkWidth then
+					table.insert(newVirtText, chunk)
+				else
+					chunkText = truncate(chunkText, targetWidth - curWidth)
+					local hlGroup = chunk[2]
+					table.insert(newVirtText, { chunkText, hlGroup })
+					chunkWidth = vim.fn.strdisplaywidth(chunkText)
+					if curWidth + chunkWidth < targetWidth then
+						suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+					end
+					break
+				end
+				curWidth = curWidth + chunkWidth
+			end
+			local rAlignAppndx = math.max(math.min(vim.opt.textwidth["_value"], width - 1) - curWidth - sufWidth, 0)
+			suffix = (" "):rep(rAlignAppndx) .. suffix
+			table.insert(newVirtText, { suffix, "MoreMsg" })
+			return newVirtText
+		end
 
-		ufo.setup({
-			provider_selector = function(bufnr, filetype, buftype)
-				return { "lsp", "indent" }
-			end,
-		})
+		opts["fold_virt_text_handler"] = handler
 
-		vim.keymap.set("n", "zR", ufo.openAllFolds, { desc = "Open all folds" })
-		vim.keymap.set("n", "zM", ufo.closeAllFolds, { desc = "Close all folds" })
-		vim.keymap.set("n", "zr", ufo.openFoldsExceptKinds)
-		vim.keymap.set("n", "zK", function()
-			local winid = ufo.peekFoldedLinesUnderCursor()
+		require("ufo").setup(opts)
+
+		vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+		vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+		vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
+		vim.keymap.set("n", "K", function()
+			local winid = require("ufo").peekFoldedLinesUnderCursor()
 			if not winid then
 				vim.lsp.buf.hover()
 			end
