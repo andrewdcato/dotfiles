519363d5854cb640e252de19e7283fa53598e85e
Andrew Cato <andrewdcato@gmail.com>
2025-11-13 09:55:20 -0600

    fix(nvim): update trouble keymaps to use new api

--- dot-config/nvim/lua/cato/plugins/trouble.lua
+++ dot-config/nvim/lua/cato/plugins/trouble.lua
@@ -2,20 +2,18 @@ return {
 	"folke/trouble.nvim",
 	dependencies = { "neovim/nvim-lspconfig" },
 	keys = {
-		{ "<leader>xx", "<cmd>TroubleToggle<cr>", desc = "Trouble: Toggle Main Window" },
 		{
-			"<leader>xw",
-			"<cmd>TroubleToggle workspace_diagnostics<cr>",
-			desc = "Trouble: Toggle Workspace Diagnostics",
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle focus=false<cr>",
+			desc = "Trouble: Toggle Document Diagnostics",
 		},
 		{
 			"<leader>xd",
 			"<cmd>Trouble diagnostics toggle focus=false filter.buf=0<cr>",
 			desc = "Trouble: Toggle Document Diagnostics",
 		},
-		{ "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", desc = "Trouble: Toggle Quickfix List" },
-		{ "<leader>xl", "<cmd>TroubleToggle lsp_references<cr>", desc = "Trouble: Toggle LSP References" },
-		{ "<leader>xL", "<cmd>TroubleToggle loclist<cr>", desc = "Trouble: Toggle LocList" },
+		{ "<leader>xq", "<cmd>Trouble toggle quickfix<cr>", desc = "Trouble: Toggle Quickfix List" },
+		{ "<leader>xl", "<cmd>Trouble toggle lsp_references<cr>", desc = "Trouble: Toggle LSP References" },
 		{ "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Trouble: View TODO Comments" },
 	},
 	opts = {
