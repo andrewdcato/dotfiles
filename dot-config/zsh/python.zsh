1ae4d21b894d7cdd9e6fc07fe3bc22aaa1b7b12f
Andrew Cato <andrewdcato@gmail.com>
2025-11-13 20:43:26 -0600

    fix(zsh): only load if installed

--- dot-config/zsh/python.zsh
+++ dot-config/zsh/python.zsh
@@ -1,6 +1,9 @@
-# Enable PyEnv 
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+# Enable PyEnv - only if installed
+if command -v pyenv &> /dev/null; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init -)"
+fi
 
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 
