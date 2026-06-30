52aa46694becd29970fcc6af4651a10c42f184db
Andrew Cato <andrewdcato@gmail.com>
2026-06-30 09:35:43 -0500

    feat(nvim): set up python lsp

---
+++ dot-config/nvim/lsp/ruff.lua
@@ -0,0 +1,10 @@
+return {
+	cmd = { "ruff", "server" },
+	filetypes = { "python" },
+	root_markers = { "pyproject.toml", "ruff.toml", ".ruff.toml", ".git" },
+	init_options = {
+		settings = {
+			-- Ruff language server settings go here
+		},
+	},
+}
